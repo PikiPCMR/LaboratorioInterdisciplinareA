8568d8ae85b2e08e599621e0e75521a73d0a8114
PikiPCMR <114292845+PikiPCMR@users.noreply.github.com>
2023-04-17 15:19:26 +0200

    fine2

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("palle");
+  public static void main(String[] args) 
+  {
+    System.out.println("vediamo come funziona");
     int x;
     System.out.println("353 + 45");
   }
