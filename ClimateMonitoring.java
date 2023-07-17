984ebf54c82b17f81bcd13868d340d2e0d454ed6
PikiPCMR <114292845+PikiPCMR@users.noreply.github.com>
2023-07-17 08:30:49 +0000

    a

--- ClimateMonitoring.java
+++ ClimateMonitoring.java
@@ -1,107 +1,166 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class ClimateMonitoring {
-        private static final String FILE_PATH = "operatori-registrati.csv";
-
-        public static void main(String[] args) throws IOException {
-            Scanner scanner = new Scanner(System.in);
-            boolean loggedIn = false;
-            String username = null;
-
-            while (true) {
-                System.out.println("Benvenuto!");
-                System.out.println("1. Ricerca");
-                System.out.println("2. Login");
-                System.out.println("3. Registrazione");
-                System.out.println("0. Esci");
-
-                int scelta = scanner.nextInt();
-                scanner.nextLine(); // Consuma il carattere di fine linea dopo la lettura del numero
-
-                switch (scelta) {
-                    case 1:
-                        if (loggedIn) {
-                            System.out.println("Effettua la tua ricerca...");
-                            // Esegui la ricerca
-                        } else {
-                            System.out.println("Effettua prima il login.");
-                        }
-                        break;
-                    case 2:
-                        if (loggedIn) {
-                            System.out.println("Sei già loggato come " + username);
-                        } else {
-                            System.out.println("Login");
-                            System.out.print("Username: ");
-                            String loginUsername = scanner.nextLine();
-                            System.out.print("Password: ");
-                            String loginPassword = scanner.nextLine();
-
-                            if (effettuaLogin(loginUsername, loginPassword)) {
-                                loggedIn = true;
-                                username = loginUsername;
-                                System.out.println("Accesso effettuato con successo!");
-                            } else {
-                                System.out.println("Username o password non validi.");
-                            }
-                        }
-                        break;
-                    case 3:
-                        if (loggedIn) {
-                            System.out.println("Sei già loggato come " + username);
-                        } else {
-                            System.out.println("Registrazione");
-                            System.out.print("Username: ");
-                            String registrazioneUsername = scanner.nextLine();
-                            System.out.print("Password: ");
-                            String registrazionePassword = scanner.nextLine();
-
-                            if (effettuaRegistrazione(registrazioneUsername, registrazionePassword)) {
-                                System.out.println("Registrazione effettuata con successo!");
-                            } else {
-                                System.out.println("Errore durante la registrazione.");
-                            }
-                        }
-                        break;
-                    case 0:
-                        System.out.println("Arrivederci!");
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Scelta non valida. Riprova.");
-                }
+    private static final String CSV_FILE = "operatori-registrati.csv";
+    private static final String CSV_SEPARATOR = ",";
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean continua = true;
+        
+        while (continua) {
+            // Mostra il menu
+            System.out.println("Benvenuto!");
+            System.out.println("1. Ricerca");
+            System.out.println("2. Login");
+            System.out.println("3. Registrazione");
+            System.out.println("0. Esci");
+            System.out.print("Scelta: ");
+            
+            int scelta = scanner.nextInt();
+            
+            switch (scelta) {
+                case 1:
+                    eseguiRicerca();
+                    break;
+                case 2:
+                    eseguiLogin();
+                    break;
+                case 3:
+                    eseguiRegistrazione();
+                    break;
+                case 0:
+                    continua = false;
+                    break;
+                default:
+                    System.out.println("Scelta non valida.");
             }
-        }
-
-        private static boolean effettuaLogin(String username, String password) throws IOException {
-            // Verifica le credenziali di login con il file CSV
-            // Esempio di implementazione semplificata:
-            try (Scanner fileScanner = new Scanner(FILE_PATH)) {
-                while (fileScanner.hasNextLine()) {
-                    String line = fileScanner.nextLine();
-                    String[] fields = line.split(",");
-
-                    if (fields.length == 2 && fields[0].equals(username) && fields[1].equals(password)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        private static boolean effettuaRegistrazione(String username, String password) {
-            // Registra un nuovo utente nel file CSV
-            // Esempio di implementazione semplificata:
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
-                writer.write(username + "," + password);
-                writer.newLine();
-                return true;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return false;
+            
+            System.out.println();
         }
     }
+    
+    private static void eseguiRicerca() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Inserisci il termine di ricerca: ");
+        String termineRicerca = scanner.nextLine();
+        
+        // Effettua la ricerca nel file CSV o in un'altra fonte di dati
+        // Esempio:
+        List<String[]> risultati = ricercaDati(termineRicerca);
+        
+        // Mostra i risultati
+        if (risultati.isEmpty()) {
+            System.out.println("Nessun risultato trovato.");
+        } else {
+            System.out.println("Risultati della ricerca:");
+            for (String[] risultato : risultati) {
+                System.out.println(Arrays.toString(risultato));
+            }
+        }
+    }
+    
+    private static void eseguiLogin() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+        
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        
+        // Verifica le credenziali nel file CSV o in un'altra fonte di dati
+        // Esempio:
+        boolean loginRiuscito = verificaCredenziali(username, password);
+        
+        if (loginRiuscito) {
+            System.out.println("Login effettuato con successo.");
+            // Esegui le operazioni dopo il login
+        } else {
+            System.out.println("Credenziali non valide.");
+        }
+    }
+    
+    private static void eseguiRegistrazione() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+        
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        
+        // Salva le credenziali nel file CSV o in un'altra fonte di dati
+        // Esempio:
+        boolean registrazioneRiuscita = salvaCredenziali(username, password);
+        
+        if (registrazioneRiuscita) {
+            System.out.println("Registrazione effettuata con successo.");
+            // Esegui le operazioni dopo la registrazione
+        } else {
+            System.out.println("Errore durante la registrazione.");
+        }
+    }
+    
+    private static List<String[]> ricercaDati(String termineRicerca) {
+        List<String[]> risultati = new ArrayList<>();
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(CSV_FILE))) {
+            String line;
+            
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(CSV_SEPARATOR);
+                
+                // Esegui la logica di ricerca sui dati
+                // Esempio: confronta il termine di ricerca con i dati presenti nel file
+                // Se trovi una corrispondenza, aggiungi l'array di stringhe ai risultati
+                // Esempio:
+                if (Arrays.asList(data).contains(termineRicerca)) {
+                    risultati.add(data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return risultati;
+    }
+    
+    private static boolean verificaCredenziali(String username, String password) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(CSV_FILE))) {
+            String line;
+            
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(CSV_SEPARATOR);
+                
+                // Verifica se le credenziali corrispondono ai dati presenti nel file
+                // Esempio:
+                if (data.length >= 2 && data[0].equals(username) && data[1].equals(password)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return false;
+    }
+    
+    private static boolean salvaCredenziali(String username, String password) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(CSV_FILE, true))) {
+            // Aggiungi una nuova riga al file CSV con le credenziali
+            // Esempio:
+            writer.write(username + CSV_SEPARATOR + password);
+            writer.newLine();
+            
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return false;
+    }
+}
