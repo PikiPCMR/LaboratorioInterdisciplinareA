dc8ea64c93bc497b6e486fefeda4ec99006f2725
PikiPCMR <114292845+PikiPCMR@users.noreply.github.com>
2023-07-17 09:34:48 +0000

    aa

--- ClimateMonitoring.java
+++ ClimateMonitoring.java
@@ -66,15 +66,15 @@ public class ClimateMonitoring {
     private static void eseguiLogin() {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Username: ");
-        String username = scanner.nextLine();
+        System.out.print("UserID: ");
+        String userID = scanner.nextLine();
         
         System.out.print("Password: ");
         String password = scanner.nextLine();
         
         // Verifica le credenziali nel file CSV o in un'altra fonte di dati
         // Esempio:
-        boolean loginRiuscito = verificaCredenziali(username, password);
+        boolean loginRiuscito = verificaCredenziali(userID, password);
         
         if (loginRiuscito) {
             System.out.println("Login effettuato con successo.");
@@ -87,15 +87,27 @@ public class ClimateMonitoring {
     private static void eseguiRegistrazione() {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Username: ");
-        String username = scanner.nextLine();
+        System.out.print("Nome e Cognome: ");
+        String nomeCognome = scanner.nextLine();
+        
+        System.out.print("Codice Fiscale: ");
+        String codFisc = scanner.nextLine();
+        
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        
+        System.out.print("UserID: ");
+        String userID = scanner.nextLine();
         
         System.out.print("Password: ");
         String password = scanner.nextLine();
         
+        System.out.print("CDMDA: ");
+        String cdmda = scanner.nextLine();
+        
         // Salva le credenziali nel file CSV o in un'altra fonte di dati
         // Esempio:
-        boolean registrazioneRiuscita = salvaCredenziali(username, password);
+        boolean registrazioneRiuscita = salvaCredenziali(nomeCognome, codFisc, email, userID, password, cdmda);
         
         if (registrazioneRiuscita) {
             System.out.println("Registrazione effettuata con successo.");
@@ -129,7 +141,7 @@ public class ClimateMonitoring {
         return risultati;
     }
     
-    private static boolean verificaCredenziali(String username, String password) {
+    private static boolean verificaCredenziali(String userID, String password) {
         try (BufferedReader reader = new BufferedReader(new FileReader(CSV_FILE))) {
             String line;
             
@@ -138,7 +150,7 @@ public class ClimateMonitoring {
                 
                 // Verifica se le credenziali corrispondono ai dati presenti nel file
                 // Esempio:
-                if (data.length >= 2 && data[0].equals(username) && data[1].equals(password)) {
+                if (data.length >= 5 && data[3].equals(userID) && data[4].equals(password)) {
                     return true;
                 }
             }
@@ -149,11 +161,12 @@ public class ClimateMonitoring {
         return false;
     }
     
-    private static boolean salvaCredenziali(String username, String password) {
+    private static boolean salvaCredenziali(String nomeCognome, String codFisc, String email, String userID, String password, String cdmda) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(CSV_FILE, true))) {
             // Aggiungi una nuova riga al file CSV con le credenziali
             // Esempio:
-            writer.write(username + CSV_SEPARATOR + password);
+            writer.write(nomeCognome + CSV_SEPARATOR + codFisc + CSV_SEPARATOR + email + CSV_SEPARATOR +
+                    userID + CSV_SEPARATOR + password + CSV_SEPARATOR + cdmda);
             writer.newLine();
             
             return true;
